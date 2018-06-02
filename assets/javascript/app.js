9b17b2f646cb9662c277b7193d341237abda8329
Josh Willems <jscottwillems@gmail.com>
2018-06-02 18:47:38 -0500

    finished project

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,36 +6,52 @@ var questionArray = ["In what year was Mario first created?", "What was Mario's 
 
 var answerArray = [["1983","1978","1988","1980"], ["Mustache Guy","Jumpman","Wario","Plumberman"], ["Super Mario Bros.","Mario Bros.","Donkey Kong","Dr. Mario"], ["Shy Guy","Phanto","Birdo","Mouser"], ["Princess Daisy","Princess Peach","Princess Diana","Princess Toadstool"], ["Carpenter","Plumber","Shoemaker","Construction Worker"], ["6","7","9","4"], ["Super Mario Bros. 3","Mario Party 2","Paper Mario","Super Mario RPG"]];
 
-// seperate images into IDs for better styling
-var imageArray = ["<img class='center-block img-right' src='assets/images/babymario.png'>", "<img class='center-block img-right' src='assets/images/jumpman.png'>", "<img class='center-block img-right' src='assets/images/donkeykong.gif'>", "<img class='center-block img-right' src='assets/images/birdo.png'>", "<img class='center-block img-right' src='assets/images/peach.png'>", "<img class='center-block img-right' src='assets/images/carpenter.png'>", "<img class='center-block img-right' src='assets/images/koopalings.png'>", "<img class='center-block img-right' src='assets/images/bowser2.gif'>"];
+var imageArray = ["<img class='center-block img-right' id='babyMario' src='assets/images/babymario.png'>", "<img class='center-block img-right' id='jumpman' src='assets/images/jumpman.png'>", "<img class='center-block img-right' id='donkeyKong' src='assets/images/donkeykong.gif'>", "<img class='center-block img-right' id='birdo' src='assets/images/birdo.png'>", "<img class='center-block img-right' id='peach' src='assets/images/peach.png'>", "<img class='center-block img-right' id='carpenter' src='assets/images/carpenter.png'>", "<img class='center-block img-right' id='koopalings' src='assets/images/koopalings.png'>", "<img class='center-block img-right' id='bowser' src='assets/images/bowser2.gif'>"];
 
 var correctAnswers = ["A. 1983", "B. Jumpman", "C. Donkey Kong", "C. Birdo", "D. Princess Toadstool", "A. Carpenter", "B. 7", "D. Super Mario RPG"];
 
 var questionCounter = 0
 var selecedAnswer;
 var theClock;
-var correctTally = 0;
-var incorrectTally = 0;
-var unansweredTally = 0;
-var clickSound = new Audio(""); //add sfx
+var numberCorrect = 0;
+var numberIncorrect = 0;
+var numberUnanswered = 0;
+
+function setVolumeLow() {
+	var myAudio = document.getElementById("background");  
+    myAudio.volume = 0.2;
+}
+
+function setVolumeNorm() {
+	var myAudio = document.getElementById("background");  
+    myAudio.volume = 1.0;
+}
+
+function initialScreen() {
+	startScreen = "<p class='text-center main-button-container'><a class='btn btn-primary btn-lg btn-block start-button' href='#' role='button'>Start Game</a></p>";
+	$(".mainArea").html(startScreen);
+}
 
 function generateLossTimeOut() {
-    unansweredTally++;
+	numberUnanswered++;
+	$('audio#incorrect')[0].play();
     gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>You ran out of time! The correct answer was: " + correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='assets/images/wrong.gif'>";
     $(".mainArea").html(gameHTML);
     setTimeout(wait, 3000);
 }
 
 function generateWin() {
-    correctTally++;
+	numberCorrect++;
+	$('audio#correct')[0].play();
     gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Correct! " + correctAnswers[questionCounter] + "</p>" + imageArray[questionCounter];
     $(".mainArea").html(gameHTML);
     setTimeout(wait, 3000);
 }
 
 function generateLoss() {
-	incorrectTally++;
-	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Wrong! The correct answer is: "+ correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='http://www.defunctgames.com/pic/deaths/rip-mario1.gif'>";
+	numberIncorrect++;
+	$('audio#incorrect')[0].play();
+	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Incorrect! The correct answer is: "+ correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='assets/images/wrong.gif'>";
 	$(".mainArea").html(gameHTML);
 	setTimeout(wait, 3000);
 }
@@ -54,33 +70,39 @@ function wait() {
 	}
 	else {
 		finalScreen();
+		setVolumeLow();
+		$('audio#gameend')[0].play();
 	}
 }
 
 function timerWrapper() {
 	theClock = setInterval(thirtySeconds, 1000);
 	function thirtySeconds() {
+		if (counter === 10) {
+			$('audio#timeup')[0].play();
+		}
 		if (counter === 0) {
 			clearInterval(theClock);
 			generateLossTimeOut();
 		}
 		if (counter > 0) {
 			counter--;
+			$(".timer").html(counter);
 		}
-		$(".timer").html(counter);
 	}
 }
 
 function finalScreen() {
-	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Let's see how you did!" + "</p>" + "<p class='summary-correct'>Correct Answers: " + correctTally + "</p>" + "<p>Wrong Answers: " + incorrectTally + "</p>" + "<p>Unanswered: " + unansweredTally + "</p>" + "<p class='text-center reset-button-container'><a class='btn btn-primary btn-lg btn-block reset-button' href='#' role='button'>Reset</a></p>";
+	
+	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Let's see how you did!" + "</p>" + "<p class='summary-correct'>Correct Answers: " + numberCorrect + "</p>" + "<p class='summary'>Incorrect Answers: " + numberIncorrect + "</p>" + "<p class='summary'>Unanswered: " + numberUnanswered + "</p>" + "<p class='text-center reset-button-container'><a class='btn btn-primary btn-lg btn-block reset-button' href='#' role='button'>Reset</a></p>";
 	$(".mainArea").html(gameHTML);
 }
 
 function resetGame() {
 	questionCounter = 0;
-	correctTally = 0;
-	incorrectTally = 0;
-	unansweredTally = 0;
+	numberCorrect = 0;
+	numberIncorrect = 0;
+	numberUnanswered = 0;
 	counter = 30;
 	generateHTML();
 	timerWrapper();
@@ -88,16 +110,15 @@ function resetGame() {
 
 $(document).ready(function() {
 
-function initialScreen() {
-    startScreen = "<p class='text-center main-button-container'><a class='btn btn-primary btn-lg btn-block start-button' href='#' role='button'>Start Game</a></p>";
-    $(".mainArea").html(startScreen);
-}
-
 initialScreen();
 
+$("body").on("load", function(){
+})
+
 $("body").on("click", ".start-button", function(event){
-    event.preventDefault();
-    clickSound.play();
+	event.preventDefault();
+	$('audio#background')[0].play();
+    $('audio#start')[0].play();
     generateHTML();
 
     timerWrapper();
@@ -105,10 +126,8 @@ $("body").on("click", ".start-button", function(event){
 });
 
 $("body").on("click", ".answer", function(event){
-	clickSound.play();
 	selectedAnswer = $(this).text();
 	if(selectedAnswer === correctAnswers[questionCounter]) {
-
 		clearInterval(theClock);
 		generateWin();
 	}
@@ -119,7 +138,8 @@ $("body").on("click", ".answer", function(event){
 });
 
 $("body").on("click", ".reset-button", function(event){
-    clickSound.play();
+	$('audio#start')[0].play();
+	setVolumeNorm();
     resetGame();
 });
 
